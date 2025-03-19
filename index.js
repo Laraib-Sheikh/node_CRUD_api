7071b1228ad4b0ca5fc479b1107d0c0b364de8bf
Daniyaladeeb <119097376+Laraib-Sheikh@users.noreply.github.com>
2025-03-19 12:00:54 +0500

    add the post api of the product

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+
 
 
 app.get('/', (req, res) => {
     res.send('Hello World pakistan');
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(res.body);
+    res.send(res.body);
+}
+)
+
+mongoose.connect('mongodb+srv://laraibahhmad:XYTJXRWfRpLLQwiE@cluster0.eiu1l.mongodb.net/node_api?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    })
+})
+.catch((error) => {
+    console.log('connect failed', error);
 })
---
+++ models/product.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+  },
+});
+
+
+const product = mongoose.model("Product", ProductSchema);
+
+module.exports = product;
